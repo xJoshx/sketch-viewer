72a57110c40dcc1d8f236e65e74001faa00b877c
Jose Vizcaíno <joseluis.vizcaino@xing.com>
2020-09-19 08:47:19 +0200

    connect with graphql

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Theme } from "./Theme";
 import { DocumentViewer } from "./DocumentViewer";
 import { ArtboardViewer } from "./ArtboardViewer";
+
+const GRAPHQL_URI = "https://graphql.sketch.cloud/api";
+
+const client = new ApolloClient({
+  uri: GRAPHQL_URI,
+  cache: new InMemoryCache(),
+});
 
 const AppWrapper = styled.div``;
 
 const App = () => {
   return (
     <Theme>
-      <AppWrapper>
-        <Router>
-          <Switch>
-            <Route exact path="/" children={<DocumentViewer />} />
-            <Route path="/artboard/:id" children={<ArtboardViewer />} />
-          </Switch>
-        </Router>
-      </AppWrapper>
+      <ApolloProvider client={client}>
+        <AppWrapper>
+          <Router>
+            <Switch>
+              <Route exact path="/" children={<DocumentViewer />} />
+              <Route path="/artboard/:id" children={<ArtboardViewer />} />
+            </Switch>
+          </Router>
+        </AppWrapper>
+      </ApolloProvider>
     </Theme>
   );
 };
--- src/DocumentViewer/DocumentViewer.jsx
+++ src/DocumentViewer/DocumentViewer.jsx
@@ -1,3 +1,46 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
-export const DocumentViewer = () => <div>Document viewer</div>;
+const FETCH_DOCUMENTS_QUERY = gql`
+  query fetchDocuments($documentId: String) {
+    share(shortId: $documentId) {
+      shortId
+      version {
+        document {
+          name
+          artboards {
+            entries {
+              name
+              isArtboard
+              files {
+                url
+                height
+                width
+                scale
+                thumbnails {
+                  url
+                  height
+                  width
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Loading = () => <div>loading...</div>;
+
+export const DocumentViewer = () => {
+  const { data, loading, error } = useQuery(FETCH_DOCUMENTS_QUERY, {
+    variables: { documentId: "Y8wDM" },
+  });
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return <div>Document viewer</div>;
+};
